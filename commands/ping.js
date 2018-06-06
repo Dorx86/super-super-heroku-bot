4edd8e958733068ae9642e029c12ec50c068b81d
Ayukito <alexbrowses@gmail.com>
2018-06-06 13:48:43 -0400

    s
    s

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     message.channel.send("Pong");
+    console.log("worker DOES run");
 }
 
 module.exports.help = {
