adf49ea9c16013a7ca37ea8871d0352b22deca01
Ayukito <alexbrowses@gmail.com>
2018-06-06 13:36:47 -0400

    reorganized
    ye

--- bot.js
+++ bot.js
@@ -12,13 +12,6 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
-
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client();
@@ -43,7 +36,7 @@ fs.readdir("./commands/", (err, files) => {
 
 
 bot.on("ready", () => {
-  console.log("I am ready!");
+  console.log("Worker is ready!");
   let channelobj = bot.channels.find("name","lounge");
   //console.log(channelobj);
 });
@@ -61,55 +54,4 @@ bot.on("message", async message => {
 
 });
 
-app.use(bodyParser.urlencoded( {extended: false }));
-app.use(bodyParser.json());
-// http://expressjs.com/en/starter/basic-routing.html
-const hook = new Discord.WebhookClient('269906337620688897','g1vigCQ2YOe3Qtg7lNKItA9iWSj33k2MV8ovfuxvrttGE9oLrNiKqMUM8pzaeUOJakUk');
-app.post("/", function (request, response) {
-  //response.sendFile(__dirname + '/views/index.html');
-  //console.log(request.headers);
-  //let msg = request.headers.message;
-  console.log("test");
-  let msg = request.body.msg;
-  hook.send(msg,{
-    username:request.body.username,
-    avatarURL:request.body.avatarURL
-  }).then(function(){
-    console.log("message sent");
-
-  }).catch(console.error);
-  response.end();
-});
-
-app.get("/lastmessage", function (request, response) {
-  //console.log(request);
-  //console.log(request.headers);
-  let channel = request.headers.channel;
-  //console.log(request);
-  console.log(channel);
-
-  let channelobj = bot.channels.find("name",channel);
-  //console.log(channelobj);
-  channelobj.fetchMessage(channelobj.lastMessageID)
-  .then(function(message){
-    console.log(message);
-    response.send({
-      channel:message.channel,
-      id:message.id,
-      member:message.member,
-      author:message.author,
-      content:message.content
-    });
-  })
-  .catch(console.error);
-});
-
 bot.login(process.env.BOT_TOKEN);
-
-hook.send("hi testing info",{
-  username:"TestThing",
-  avatarURL:"https://americanaddictioncenters.org/wp-content/uploads/2017/07/transgender_icon.jpg",
-}).then(function(){
-    console.log("message sent");
-
-  }).catch(console.error);
